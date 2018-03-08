d42cd8c7f534d67e2b48aaa266df8733a609ddc0
_unkn0wm_ <muntasir.shuaib@gmail.com>
2018-03-08 17:49:05 +0300

    Demo Done.

--- TextValidation/ViewController.swift
+++ TextValidation/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userTickImage: UIImageView!
+    @IBOutlet weak var passwordTickImage: UIImageView!
+    
+    @IBOutlet weak var usernameTxtFielf: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupView()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func signInBtn(_ sender: Any) {
+        
+        // For the username check
+        if usernameTxtFielf.text != "" {
+            userTickImage.image = UIImage(named:"btn_success.png")
+            userTickImage.isHidden = false
+        } else {
+            userTickImage.image = UIImage(named:"btn_wrong.png")
+            userTickImage.isHidden = false
 
+        }
+        
+        // For the password check
+        if passwordTextField.text != "" {
+            passwordTickImage.image = UIImage(named:"btn_success.png")
+            passwordTickImage.isHidden = false
 
+        } else {
+            passwordTickImage.image = UIImage(named:"btn_wrong.png")
+            passwordTickImage.isHidden = false
+
+        }
+    }
+    
+    func setupView(){// Hiding the imageViews by default
+        userTickImage.isHidden = true
+        passwordTickImage.isHidden = true
+    }
 }
 
